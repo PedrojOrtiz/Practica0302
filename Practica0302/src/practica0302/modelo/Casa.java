9b1b1027cd0a94308b3bdf2a96538c09dd93b7c4
pedro <pedro@172.16.221.100>
2018-01-10 17:41:32 -0500

    Hagale

---
+++ Practica0302/src/practica0302/modelo/Casa.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica0302.modelo;
+
+/**
+ *
+ * @author pedro
+ */
+public class Casa {
+    
+    private int id;
+    private Propietario propietario;
+    private String ubicacion;
+
+    public Casa(int id, Propietario propietario, String ubicacion) {
+        this.id = id;
+        this.propietario = propietario;
+        this.ubicacion = ubicacion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Casa{" + "id=" + id + ", propietario=" + propietario + ", ubicacion=" + ubicacion + '}';
+    }
+    
+    
+    
+}
---
+++ Practica0302/src/practica0302/modelo/Propietario.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica0302.modelo;
+
+/**
+ *
+ * @author pedro
+ */
+public class Propietario {
+    
+    private int id;
+    private String nombre;
+    private int edad;
+    private String cedula;
+
+    public Propietario(int id, String nombre, int edad, String cedula) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.cedula = cedula;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    @Override
+    public String toString() {
+        return "Propietario{" + "id=" + id + ", nombre=" + nombre + ", edad=" + edad + ", cedula=" + cedula + '}';
+    }
+    
+    
+    
+}
